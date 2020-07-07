dc0b0068dceae529b1884563f0160004029537e1
adrian <revanskyw@gmail.com>
2020-07-07 14:44:50 -0400

    formulario js

---
+++ script.js
@@ -0,0 +1,93 @@
+//variables
+
+
+
+function validar() {
+
+    let nombre, apellidos, correo, usuario, clave, telefono, expresion;
+    nombre = document.getElementById("nombre");
+    apellidos = document.getElementById("apellidos");
+    correo = document.getElementById("correo");
+    usuario = document.getElementById("usuario");
+    clave = document.getElementById("pass");
+    telefono = document.getElementById("telefono");
+
+
+
+
+    console.log(nombre.value);
+    console.log(apellidos.value);
+    console.log(correo);
+    console.log(usuario);
+    console.log(pass);
+    console.log(telefono);
+
+    //validaciones
+
+    if (
+        nombre.value === "" ||
+        apellidos.value === "" ||
+        correo.value === "" ||
+        usuario.value === "" ||
+        usuario.value === "" ||
+        pass.value === "" ||
+        telefono.value === ""
+    ) {
+        alert("todos los campos son obligatorios");
+        return false;
+
+    }
+
+
+    else if (nombre.value.length > 30) {
+
+        alert("el nombre es muy largo");
+        return false;
+    }
+    else if (apellidos.value.length > 80) {
+
+        alert("los apellidos son muy largos");
+        return false;
+    }
+
+    else if (correo.value.length > 100) {
+
+        alert("correo muy largo");
+        return false;
+    }
+
+    else if (usuario.value.length > 20) {
+
+        alert("nombre de usuario muy largo");
+        return false;
+
+    }
+
+    else if (telefono.value.length > 15) {
+
+        alert("El teléfono es muy largo");
+        return false;
+    }
+    else if (isNaN(telefono.value)) {
+
+        alert("el telefono debe ser solo numeros");
+        return false;
+    }
+    else {
+
+        alert("bienvenido " + nombre.value + " " + apellidos.value + " gracias por registrarte");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
